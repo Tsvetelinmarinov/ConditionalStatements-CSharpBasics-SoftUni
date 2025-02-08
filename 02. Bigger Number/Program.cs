985dd3026c8e920a806c6462160c611304d06f3e
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-02-08 21:52:04 +0200

    Задача две - По голямото число

---
+++ 02. Bigger Number/Program.cs
@@ -0,0 +1,16 @@
+﻿/**
+ * Bigger number
+ */
+
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(secondNumber);
+}
