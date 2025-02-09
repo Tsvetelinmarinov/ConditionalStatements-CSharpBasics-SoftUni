665db52b0bdc120e27c57e8f54c6d32025af003c
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-02-09 17:34:11 +0200

    Задачи 6 и 7

---
+++ 06. Speed Info/Program.cs
@@ -0,0 +1,47 @@
+﻿/**
+ * 06. Speed Info
+ */
+
+
+/**
+•	При скорост до 10 (включително) отпечатайте "slow"
+•	При скорост над 10 и до 50 (включително) отпечатайте "average" 
+•	При скорост над 50 и до 150 (включително) отпечатайте "fast"
+•	При скорост над 150 и до 1000 (включително) отпечатайте "ultra fast" 
+•	При по-висока скорост отпечатайте "extremely fast"
+*/
+
+
+/**
+ * 8	slow		49.5	average		126	fast		160	ultra fast		3500	extremely fast
+ */
+
+
+
+double speed = double.Parse(Console.ReadLine());
+string outputMessage = "";
+
+
+if (speed <= 10)
+{
+    outputMessage = "slow";
+}
+else if (speed > 10 && speed <= 50)
+{
+    outputMessage = "average";
+}
+else if (speed > 50 && speed <= 150)
+{
+    outputMessage = "fast";
+}
+else if (speed > 150 && speed <= 1000)
+{
+    outputMessage = "ultra fast";
+}
+else
+{
+    outputMessage = "extremely fast";
+}
+
+
+Console.WriteLine(outputMessage);
---
+++ 07. Area of Figures/Program.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * 07. Area of Figures
+ */
+
+
+// square, rectangle, circle или triangle
+
+double figureArea = 0.0;
+
+
+string figureType = Console.ReadLine();
+
+
+
+if (figureType == "square")
+{
+    double squareSide = double.Parse(Console.ReadLine());
+    figureArea = Math.Pow(squareSide, 2);
+}
+else if (figureType == "rectangle")
+{
+    double squareLength = double.Parse(Console.ReadLine());
+    double squareHeigth = double.Parse(Console.ReadLine());
+    figureArea = squareLength * squareHeigth;
+}
+else if (figureType == "circle")
+{
+    double circleRadius = double.Parse(Console.ReadLine());
+    figureArea = Math.PI * Math.Pow(circleRadius, 2);
+}
+else if (figureType == "triangle")
+{
+    double triangleSide = double.Parse(Console.ReadLine());
+    double heigth = double.Parse(Console.ReadLine());
+    figureArea = triangleSide * heigth / 2;
+}
+
+
+
+Console.WriteLine($"{figureArea:F3}");
